8fd2d435facab285cc4c5527fa2d6e6b61c258c3
minenyaraya <minenyaraya@gmail.com>
2026-02-27 13:36:05 -0600

    Add gitignore and update package

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path"; // Agrega esta línea
+import path from "path"; 
 import staticRoutes from "./routes/routes.js";
 
 dotenv.config();
 const app = express();
 app.set("view engine", "ejs");
 
-// Cambia la línea de public por esta:
 app.use(express.static(path.join(process.cwd(), "public")));
 
 const port = process.env.PORT || 3000;
